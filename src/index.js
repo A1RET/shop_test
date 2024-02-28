8b1cafd7515147933d81bf154fce5b32bb0d47b6
Airet <thiefon@gmail.com>
2024-02-28 13:50:49 +0500

    Добавил store.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from 'react-redux';
 
-import App from './App'
+import App from "./components/app/App";
+import store from './store';
 
-import './styles/index.css'
+import "./styles/index.css";
 
-const rootView = document.getElementById('root')
+const rootView = document.getElementById("root");
 
 if (rootView) {
-  ReactDOM.render(
+  const root = createRoot(rootView);
+
+  root.render(
     <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-    rootView
-  )
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </React.StrictMode>
+  );
 }
